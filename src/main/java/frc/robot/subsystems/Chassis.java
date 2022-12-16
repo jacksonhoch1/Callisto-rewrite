40b18bfdf49c77d17cfed9ddf97ec154c5c51616
Jacksonhoch1 <jacksonhoch1@gmail.com>
2022-12-16 18:25:02 -0500

    added contructor to `Chassis.java`

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -4,11 +4,73 @@
 
 package frc.robot.subsystems;
 
+//vendor libraries
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import com.kauailabs.navx.frc.AHRS;
+
+
+//wpi libraries
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.SPI;
+
+//utils
+import frc.robot.utils.IDMap;
 
 public class Chassis extends SubsystemBase {
+  WPI_TalonFX m_leftFront, m_leftRear, m_rightFront, m_rightRear;
+  DifferentialDrive m_differentialDrive;
+  AHRS m_gyro;
   /** Creates a new Chassis. */
-  public Chassis() {}
+  public Chassis() {
+    //TalonFX setup
+    TalonFXConfiguration configs = new TalonFXConfiguration();
+    configs.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
+
+    //left front
+    m_leftFront = new WPI_TalonFX(IDMap.CAN.LEFT_FRONT.ID);
+    m_leftFront.configFactoryDefault();
+    m_leftFront.configAllSettings(configs);
+    m_leftFront.setInverted(TalonFXInvertType.Clockwise);
+    m_leftFront.setNeutralMode(NeutralMode.Brake);
+
+    //right Front
+    m_rightFront = new WPI_TalonFX(IDMap.CAN.RIGHT_FRONT.ID);
+    m_rightFront.configFactoryDefault();
+    m_rightFront.configAllSettings(configs);
+    m_rightFront.setInverted(TalonFXInvertType.CounterClockwise);
+    m_rightFront.setNeutralMode(NeutralMode.Brake);
+
+    //left rear
+    m_leftRear = new WPI_TalonFX(IDMap.CAN.LEFT_REAR.ID);
+    m_leftRear.configFactoryDefault();
+    m_leftRear.configAllSettings(configs);
+    m_leftRear.follow(m_leftFront);
+    m_leftRear.setInverted(TalonFXInvertType.FollowMaster);
+    m_leftRear.setNeutralMode(NeutralMode.Brake);
+
+    //right rear
+    m_rightRear = new WPI_TalonFX(IDMap.CAN.RIGHT_REAR.ID);
+    m_rightRear.configFactoryDefault();
+    m_rightRear.configAllSettings(configs);
+    m_rightRear.follow(m_rightFront);
+    m_rightRear.setInverted(TalonFXInvertType.FollowMaster);
+    m_rightRear.setNeutralMode(NeutralMode.Brake);
+
+
+    //Gyro setup
+    m_gyro = new AHRS(SPI.Port.kMXP);
+    m_gyro.reset();
+
+
+    //differential drive setup
+    m_differentialDrive = new DifferentialDrive(m_leftFront, m_rightFront);
+  }
 
   @Override
   public void periodic() {
