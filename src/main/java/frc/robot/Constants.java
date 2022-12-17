8784c77b711cba91ec6aa0bdf1cf9d0766a56bc2
Jacksonhoch1 <jacksonhoch1@gmail.com>
2022-12-16 21:41:31 -0500

    added chassis constants to `Constants.java`

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,4 +27,18 @@ public final class Constants {
 
     public static final double INDEXER_SPEED = 0.5;
     public static final double INDEXER_REVERSE_SPEED = -0.2;
+
+    //chassis constants
+    public static double WHEEL_DIAMETER_INCHES = 4; 
+    public static double WHEEL_DIAMETER_METERS = 0.1016;  
+    public static double TRACK_WIDTH = 0;
+
+    public static double GEAR_RATIO = 9.64;
+
+    public static double ENCODER_COUNTS_PER_REVOLUTION = 2048; // this value is from the manufacturer
+    public static double COUNTS_PER_REVOLUTION = ENCODER_COUNTS_PER_REVOLUTION * GEAR_RATIO; // this value incorporates the gear ratio and equals 19742.72
+
+    public static double COUNTS_PER_INCH = COUNTS_PER_REVOLUTION / Math.PI * WHEEL_DIAMETER_INCHES;
+    public static double COUNTS_PER_METER = COUNTS_PER_REVOLUTION / (Math.PI * WHEEL_DIAMETER_METERS);
+
 }
