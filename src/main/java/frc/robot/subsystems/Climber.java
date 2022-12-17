974dc35bfda10090a7b63f92d2751f9a41746846
Jacksonhoch1 <jacksonhoch1@gmail.com>
2022-12-16 22:33:02 -0500

    climb subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,11 +4,35 @@
 
 package frc.robot.subsystems;
 
+//robot files
+import frc.robot.utils.IDMap;
+
+//wpi libraries
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//vendor libraries
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Climber extends SubsystemBase {
+  private CANSparkMax m_leftClimb, m_rightClimb;
   /** Creates a new Climber. */
-  public Climber() {}
+  public Climber() {
+    //left motor
+    m_leftClimb = new CANSparkMax(IDMap.CAN.CLIMBER_LEFT.ID, MotorType.kBrushless);
+    m_leftClimb.restoreFactoryDefaults();
+    m_leftClimb.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+    //right motor
+    m_rightClimb = new CANSparkMax(IDMap.CAN.CLIMBER_RIGHT.ID, MotorType.kBrushless);
+    m_rightClimb.restoreFactoryDefaults();
+    m_rightClimb.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    m_rightClimb.follow(m_leftClimb);
+  }
+
+  public void setSpeed(double speed) {
+    m_leftClimb.set(speed);
+  }
 
   @Override
   public void periodic() {
